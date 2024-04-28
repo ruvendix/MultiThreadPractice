5149047a5d50508a1de8de96494a609470240611
Ruvendix <ruvendix@naver.com>
2024-04-28 16:42:53 +0900

    Lock-based와 Lock-free 비교

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,34 +1,95 @@
 #include <iostream>
-#include <future>
 #include <thread>
 #include <chrono>
+#include <mutex>
+#include <atomic>
 
-int ThreadFunc()
+int g_num = 0;
+
+// 락프리
+class SpinLock
 {
-	// 해당 쓰레드에서 4초 쉼
-	std::this_thread::sleep_for(std::chrono_literals::operator""s(4ull));
-	return 10;
+public:
+	SpinLock() = default;
+	~SpinLock() = default;
+	
+	void Lock()
+	{
+		bool bExpected = false;
+		bool bDesire = true;
+
+		/*
+		m_lockFlag와 bExpected가 같으면 m_lockFlag를 bDesire로 설정하고 true 반환
+		m_lockFlag와 bExpected가 다르면 bExpected를 m_lockFlag로 설정하고 false 반환
+		이러한 구조로 스핀락을 구현할 때는 스핀이 false를 반환하면 bExpected를 false로 넣어야함
+		*/
+		while (m_lockFlag.compare_exchange_strong(bExpected, bDesire) == false)
+		{
+			bExpected = false;
+		}
+	}
+
+	void Unlock()
+	{
+		m_lockFlag.store(false);
+	}
+
+private:
+	std::atomic<bool> m_lockFlag = false;
+};
+
+void LockThreadFunc(std::mutex* pMutex)
+{
+	std::lock_guard<std::mutex> lock(*pMutex);
+	for (int i = 0; i < 10000000; ++i)
+	{
+		++g_num;
+	}
+}
+
+void SpinLockThreadFunc(SpinLock* pSpinLock)
+{	
+	pSpinLock->Lock();
+	for (int i = 0; i < 10000000; ++i)
+	{
+		++g_num;
+	}
+	pSpinLock->Unlock();
 }
 
 int main()
 {
-	std::future<int> future = std::async(std::launch::async, &ThreadFunc);
+#pragma region 락
+	auto lockStartTime = std::chrono::steady_clock::now();
 
-	auto timePoint = std::chrono::steady_clock::now();
+	std::mutex mutex;
+	std::thread lockThread1(LockThreadFunc, &mutex);
+	std::thread lockThread2(LockThreadFunc, &mutex);
 
-	// 4초 동안 딴 짓 가능함
-	for (int i = 0; i < 100000; ++i)
-	{
-		for (int j = 0; j < 200000; ++j)
-		{
+	lockThread1.join();
+	lockThread2.join();
 
-		}
-	}
+	auto diffLockTime = (std::chrono::steady_clock::now() - lockStartTime);
+	std::cout << "락 결과: " << g_num << std::endl;
+	std::cout << "경과 시간: " << std::chrono::duration<float>(diffLockTime).count() << "초" << std::endl << std::endl;
+#pragma endregion
 
-	auto diffTimePoint = std::chrono::steady_clock::now() - timePoint;
+	g_num = 0;
 
-	std::cout << "걸린 시간: " << std::chrono::duration<float>(diffTimePoint).count() << std::endl;
-	std::cout << future.get(); // 비동기로 실행중
-	
+#pragma region 스핀락
+	auto spinLockStartTime = std::chrono::steady_clock::now();
+
+	SpinLock spinLock;
+	std::thread spinLockThread1(SpinLockThreadFunc, &spinLock);
+	std::thread spinLockThread2(SpinLockThreadFunc, &spinLock);
+
+	spinLockThread1.join();
+	spinLockThread2.join();
+
+	auto diffSpinLockTime = (std::chrono::steady_clock::now() - spinLockStartTime);
+	std::cout << "스핀락 결과: " << g_num << std::endl;
+	std::cout << "경과 시간: " << std::chrono::duration<float>(diffSpinLockTime).count() << "초" << std::endl;
+#pragma endregion
+
 	return 0;
 }
